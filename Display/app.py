035efab84e34fa6ffdbe05c3e9c61f8a13d2f062
Joe Pearson <joe.pearson@weather.com>
2019-05-23 13:43:51 -0400

    debug

--- Display/app.py
+++ Display/app.py
@@ -114,7 +114,7 @@ def on_message(client, userdata, msg):
     print(shortcast)
     print(iconcode)
     icon_cond = icon_dict[iconcode]
-    
+    print(type(icon_cond))
     if type(icon_cond) == "str":
         display_condition(icon_cond)
         msgPayload = create_message("display", icon_cond, "")
